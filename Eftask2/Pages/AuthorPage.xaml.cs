9499d13def4c3f6bee5758e87d8b4ae0d80c2346
Amirl_kl34@itstep.edu.az <Amirl_kl34@itstep.edu.az>
2024-07-14 15:10:59 +0400

    second

---
+++ Eftask2/Pages/AuthorPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eftask2.Pages
+{
+    /// <summary>
+    /// Interaction logic for AuthorPage.xaml
+    /// </summary>
+    public partial class AuthorPage : Page
+    {
+        public AuthorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
